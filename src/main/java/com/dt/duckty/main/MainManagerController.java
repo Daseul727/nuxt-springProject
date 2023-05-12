9df4b973c27a5c7db830abf06e1da42c0a175c5c
김다슬 <daseul727@gmail.com>
2023-05-12 10:41:21 +0900

    [김다슬] thymeleaf layout 적용

--- src/main/java/com/dt/duckty/main/MainManagerController.java
+++ src/main/java/com/dt/duckty/main/MainManagerController.java
@@ -3,13 +3,14 @@ package com.dt.duckty.main;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainManagerController {
 
     @RequestMapping("/admin")
-    public String index(Model model) {
-        model.addAttribute("hello","서버통신 완료우");
-        return "/index";
+    public ModelAndView index(ModelAndView modelAndView) {
+        modelAndView.setViewName("index");
+        return modelAndView;
     }
 }
